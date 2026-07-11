958646071469b9c95fb898143e713f4eb6595780
Estobam989 <williamsoluwatosin88@gmail.com>
2026-07-11 20:19:22 +0100

    Add files via upload

---
+++ config (1).py
@@ -0,0 +1,20 @@
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+
+MODEL_NAME = os.getenv(
+    "MODEL_NAME",
+    "llama-3.3-70b-versatile"
+)
+
+TEMPERATURE = float(
+    os.getenv("TEMPERATURE", 0.3)
+)
+
+MAX_TOKENS = int(
+    os.getenv("MAX_TOKENS", 4096)
+)
